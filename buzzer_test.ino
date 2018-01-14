761ebfcf5ce13f3d6f70f0a681ef326d93c54ad8
BabakAsad <bobi.asad@gmail.com>
2018-01-13 20:14:47 -0800

    revision two

--- buzzer_test.ino
+++ buzzer_test.ino
@@ -1,8 +1,10 @@
+
 void setup() {
   // put your setup code here, to run once:
 pinMode(5,OUTPUT);
 }
-
+//Put this on github
+//This is a test for buzzer health
 void loop() {
   // put your main code here, to run repeatedly:
 digitalWrite(5, HIGH);
